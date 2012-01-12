7b7411c820de78da607bd097a0228b682f252b2e
Brian Lau <ir00ber@gmail.com>
2012-01-12 06:31:06 -0700

    Initial commit

---
+++ myIO/src/myIO.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class myIO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		System.out.println("Please enter the path of your file:");
+		Scanner scan = new Scanner(System.in);
+		String file = scan.nextLine();
+		BufferedReader inputStream = null;
+		PrintWriter outputStream = null;
+
+		try {
+			inputStream = new BufferedReader(new FileReader(file));
+			outputStream = new PrintWriter(new FileWriter("output.txt"));
+
+			List<String> text = new ArrayList<String>();
+			List<String> revText = new ArrayList<String>();
+			String l,m,n;
+			while ((l = inputStream.readLine()) != null) {
+				text.add(l);
+			}
+			for(int i = 0; i <= text.size()-1; i++){
+				String temp = text.get(i);
+				String rev ="";
+				for(int j = temp.length()-1; j >=0; j--){
+					char letter = temp.charAt(j);
+					rev = rev + letter;
+				}
+				revText.add(rev.toString());
+			}
+//			while ((m = inputStream.readLine()) != null){
+//				for(int i = m.length()-1; i >= 0; i--){
+//					outputStream.print(m.charAt(i));
+//				}
+//			}
+		} 
+		catch (Exception e){
+			System.out.println("File Not Found");
+		}
+		finally {
+			if (inputStream != null) {
+				inputStream.close();
+			}
+			if (outputStream != null) {
+				outputStream.close();
+			}
+		}
+	}
+}
